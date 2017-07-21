e0211e6216cb321409f7c552d6c1bf2213d75f0f
Joel Dahne <joda2329@student.uu.se>
2017-07-21 12:00:39 +0200

    New functions for getting coefficients and derivatives
    @taylor/coefs.m: New function for getting the coefficients of a Taylor
    expansion.
    @taylor/deriv.m: New function for getting the derivatives of a Taylor
    expansion.

---
+++ inst/@taylor/coefs.m
@@ -0,0 +1,68 @@
+## Copyright 2017 Joel Dahne
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @documentencoding UTF-8
+## @deftypeop {@@taylor} coefs (@var{X})
+## @deftypeopx {@@taylor} coefs (@var{X}, @var{N})
+## @comment DO NOT SYNCHRONIZE DOCUMENTATION STRING
+##
+## Get the coefficients of degree @{N} of the taylor expansion
+## @var{X}.  @var{N} can be a vector and it then gets the coefficient
+## for every element in the vector.  If @var{N} is not given, get all
+## coefficients.
+##
+## @example
+## @group
+## coefs (taylor ([1; 2], 2))
+##   @result{} ans = 1  2
+##                   0  0
+##                   0  0
+## @end group
+## @end example
+## @seealso{@@taylor/order}
+## @end deftypeop
+
+## Author: Joel Dahne
+## Keywords: taylor
+## Created: 2017-07-21
+
+function result = coefs (x, n)
+
+  if (nargin != 1 && nargin != 2)
+    print_usage ();
+    return
+  endif
+
+  if (nargin == 1)
+    result = x.coefs;
+  else
+    s = size (x.coefs);
+    s(1) = length (n);
+    result = x.coefs(n + 1, :);
+    result = reshape (result, s);
+  endif
+
+endfunction
+
+%!# from the documentation string
+%!assert (coefs (taylor ([1; 2], 2)), [1, 2; 1, 1; 0, 0])
+
+%!test
+%! x = taylor (magic (3), 2);
+%! assert (coefs (x, 0), reshape (magic (3), [1 3 3]));
+%! assert (coefs (x, 1), reshape (ones (3), [1 3 3]));
+%! assert (coefs (x, 2), reshape (zeros (3), [1 3 3]));
+%! assert (coefs (x, [1 1 2]), cat (1, ones (1, 3, 3), ones (1, 3, 3), zeros (1, 3, 3)));
---
+++ inst/@taylor/deriv.m
@@ -0,0 +1,76 @@
+## Copyright 2017 Joel Dahne
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @documentencoding UTF-8
+## @defmethod {@@taylor} deriv (@var{X}, @var{N})
+##
+## Get the @var{N}-th derivative of the Tayor expansion @var{X}.
+## @var{N} can be a vector and it then gets the derivative for every
+## element in the vector.  If @var{N} is not given, get all
+## derivatives.
+##
+## @example
+## @group
+## deriv (taylor ([1; 2]), 1)
+##   @result{} ans = 2
+## @end group
+## @end example
+## @seealso{@@taylor/get_order}
+## @end defmethod
+
+## Author: Joel Dahne
+## Keywords: taylor arithmetic
+## Created: 2017-03-05
+
+function result = deriv (x, n)
+
+  if (nargin != 1 && nargin != 2)
+    print_usage ();
+    return
+  endif
+
+
+  if (nargin == 1)
+    d = coefs (x);
+    n = (0 : size (d, 1) - 1)';
+  elseif (nargin == 2)
+    d = coefs (x, n);
+    n = n(:);
+  endif
+
+  if (isa (d, "infsupdec"))
+    n = infsupdec (n);
+  elseif (isa (d, "infsup"))
+    n = infsup (n);
+  endif
+
+  result = d .* factorial (n);
+endfunction
+
+%!test
+%! x = taylor (ones (5, 1));
+%! assert (deriv (x), [1; 1; 2; 6; 24]);
+%! assert (deriv (x, 0), 1)
+%! assert (deriv (x, 1), 1)
+%! assert (deriv (x, 2), 2)
+%! assert (deriv (x, 3), 6)
+%! assert (deriv (x, 4), 24)
+%!test
+%! x = taylor (magic (3), 2);
+%! assert (deriv (x, 0), reshape (magic (3), [1 3 3]));
+%! assert (deriv (x, 1), ones (1, 3, 3));
+%! assert (deriv (x, 2), zeros (1, 3, 3));
+%! assert (deriv (x, [1 1 2]), cat (1, ones (1, 3, 3), ones (1, 3, 3), zeros (1, 3, 3)));
