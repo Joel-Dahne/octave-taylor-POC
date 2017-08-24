7583b3152d6b971e7f8bff284d77c34298023529
Joel Dahne <joda2329@student.uu.se>
2017-08-24 14:06:31 +0200

    @taylor/cos.m, @taylor/sin.m: Rewritten functions

--- inst/@taylor/cos.m
+++ inst/@taylor/cos.m
@@ -1,4 +1,4 @@
-## Copyright 2014-2016 Joel Dahne
+## Copyright 2017 Joel Dahne
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -15,42 +15,49 @@
 
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
-## @documentencoding UTF-8
 ## @defmethod {@@taylor} cos (@var{X})
-## 
-## Compute the sine in radians.
+##
+## Compute the sine function in radians
 ##
 ## @example
 ## @group
-## sin (taylor ([1, 2]))
-##   @result{} ans = 
+## cos (taylor (infsupdec (0), 3))
+##   @result{} ans = [1]_com + [0]_com X + [-0.5]_com X^2 + [0]_com X^3
 ## @end group
 ## @end example
-## @seealso{@@taylor/cos}
+## @seealso{@@taylor/sin}
 ## @end defmethod
 
 ## Author: Joel Dahne
-## Keywords: taylor arithmetic
-## Created: 2017-03-05
+## Keywords: taylor
+## Created: 2017-08-24
 
-function x = cos (x)
+function result_cos = cos (x)
 
-  if (nargin ~= 1)
+  if (nargin != 1)
     print_usage ();
     return
   endif
 
-  order = get_order (x);
-  ## Need to compute taylor series for both sin and cos
-  sin_coefs = sin (x.coefs (1));
-  cos_coefs = cos (x.coefs (1));
-  sin_coefs = resize (sin_coefs, order+1, 1);
-  cos_coefs = resize (cos_coefs, order+1, 1);
-  
-  for k = [1:order]
-    sin_coefs (k+1) = dot ((1:k)'.*x.coefs(2:k+1), cos_coefs(k:-1:1)) ./ k;
-    cos_coefs (k+1) = -dot ((1:k)'.*x.coefs(2:k+1), sin_coefs(k:-1:1)) ./ k;
+  result_sin = x;
+  result_cos = x;
+
+  result_sin.coefs(1, :) = sin (x.coefs(1, :));
+  result_cos.coefs(1, :) = cos (x.coefs(1, :));
+
+  for k = 1:order (x)
+    result_sin.coefs(k+1, :) = dot ((1:k)'.*x.coefs(2:k+1, :),
+                                    result_cos.coefs(k:-1:1, :), 1)./k;
+    result_cos.coefs(k+1, :) = -dot ((1:k)'.*x.coefs(2:k+1, :),
+                                     result_sin.coefs(k:-1:1, :), 1)./k;
   endfor
 
-  x.coefs = cos_coefs;
 endfunction
+
+%!assert (isequal (cos (taylor (infsupdec (0), 4)), taylor (infsupdec ("1; 0; -1/2; 0; 1/24"))));
+%!assert (isequal (cos (taylor (infsupdec ([0; 1; 1; 0]))), taylor (infsupdec ("1; 0; -1/2; -1"))));
+
+%!test
+%! x = taylor (infsupdec ([0, 0; 1, 1; 0, 1; 0, 0; 0, 0]));
+%! y = taylor (infsupdec ("1, 1; 0, 0; -1/2, -1/2; 0, -1; 1/24, -11/24"));
+%! assert (isequal (cos (x), y));
--- inst/@taylor/sin.m
+++ inst/@taylor/sin.m
@@ -1,4 +1,4 @@
-## Copyright 2014-2016 Joel Dahne
+## Copyright 2017 Joel Dahne
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -15,42 +15,48 @@
 
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
-## @documentencoding UTF-8
 ## @defmethod {@@taylor} sin (@var{X})
-## 
-## Compute the sine in radians.
+##
+## Compute the sine function in radians
 ##
 ## @example
 ## @group
-## sin (taylor ([1, 2]))
-##   @result{} ans = 
+## sin (taylor (infsupdec (0), 3))
+##   @result{} ans = [0]_com + [1]_com X + [0]_com X^2 + [-0.16667, -0.16666]_com X^3
 ## @end group
 ## @end example
-## @seealso{@@taylor/asin, @@taylor/csc, @@taylor/sinh}
+## @seealso{@@taylor/cos}
 ## @end defmethod
 
 ## Author: Joel Dahne
-## Keywords: taylor arithmetic
-## Created: 2017-03-05
+## Keywords: taylor
+## Created: 2017-08-24
 
-function x = sin (x)
+function result_sin = sin (x)
 
-  if (nargin ~= 1)
+  if (nargin != 1)
     print_usage ();
     return
   endif
 
-  order = get_order (x);
-  ## Need to compute taylor series for both sin and cos
-  sin_coefs = sin (x.coefs (1));
-  cos_coefs = cos (x.coefs (1));
-  sin_coefs = resize (sin_coefs, order+1, 1);
-  cos_coefs = resize (cos_coefs, order+1, 1);
-  
-  for k = [1:order]
-    sin_coefs (k+1) = dot ((1:k)'.*x.coefs(2:k+1), cos_coefs(k:-1:1)) ./ k;
-    cos_coefs (k+1) = -dot ((1:k)'.*x.coefs(2:k+1), sin_coefs(k:-1:1)) ./ k;
+  result_sin = x;
+  result_cos = x;
+
+  result_sin.coefs(1, :) = sin (x.coefs(1, :));
+  result_cos.coefs(1, :) = cos (x.coefs(1, :));
+  for k = 1:order (x)
+    result_sin.coefs(k+1, :) = dot ((1:k)'.*x.coefs(2:k+1, :),
+                                    result_cos.coefs(k:-1:1, :), 1)./k;
+    result_cos.coefs(k+1, :) = -dot ((1:k)'.*x.coefs(2:k+1, :),
+                                     result_sin.coefs(k:-1:1, :), 1)./k;
   endfor
 
-  x.coefs = sin_coefs;
 endfunction
+
+%!assert (isequal (sin (taylor (infsupdec (0), 4)), taylor (infsupdec ("0; 1; 0; -1/6; 0"))));
+%!assert (isequal (sin (taylor (infsupdec ([0; 1; 1; 0]))), taylor (infsupdec ("0; 1; 1; -1/6"))));
+
+%!test
+%! x = taylor (infsupdec ([0, 0; 1, 1; 0, 1; 0, 0; 0, 0]));
+%! y = taylor (infsupdec ("0, 0; 1, 1; 0, 1; -1/6, -1/6; 0, -1/2"));
+%! assert (isequal (sin (x), y));
